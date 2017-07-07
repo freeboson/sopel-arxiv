89534aa82fb4a87d2f6ab82d7c65eb6f1da34691
Sujeet Akula <sujeet@freeboson.org>
2017-07-07 14:41:12 +1000

    clean up regex, httpsify result

--- arxiv.py
+++ arxiv.py
@@ -18,19 +18,38 @@ arxiv_catch = re.compile(
         (http[s]?://[^/]*xxx\.lanl\.gov/[a-z]+/|    # old domain
          http[s]?://[^/]*arxiv\.org/[a-z]+/|        # main
          arxiv:)                                    # common shorthand
-        (\d{4}\.\d{4,5}|[a-z\-\.]+/\d{7}) # arXiv id in group(2)""",
-    re.X)
+        (                                           # arXiv id in group(2)
+            \d{4}\.\d{4,5}                          # new and newer fmts
+        |
+            [\w\-\.]+/\d{7}                         # old format
+        )
+    """, re.X)
 
 # Base api query url
 base_url = 'https://export.arxiv.org/api/query'
 
-feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
-feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
+feedparser._FeedParserMixin.namespaces['https://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
+feedparser._FeedParserMixin.namespaces['https://arxiv.org/schemas/atom'] = 'arxiv'
 
-id_filter = re.compile(r'.*\/abs\/((?:[\d\.]{9})|(?:[\w\-\.]*\/\d{7}))(?:v\d+){,1}[/]{,1}') # id will be in \1 
+id_filter = re.compile(
+    r"""
+        .*\/abs\/                                   # gives abs link
+        (                                           # arXiv id in \1
+            \d{4}\.\d{4,5}                          # new and newer fmts
+        |
+            [\w\-\.]+/\d{7}                         # old format
+        )
+        (v\d+){,1}                                  # get rid of version
+        [/]{,1}                                     # "   "   "  trailing slash
+    """, re.X)
+
 collab_check = re.compile(r'\s(?:Collaboration)|(?:Group).*', flags=re.IGNORECASE)
-no_http = re.compile(r'.*\/\/(.*)')
+
 no_newlines = re.compile(r'\n')
+http = re.compile(r'http:')
+
+def httpsify(url):
+    http.sub('https:', url)
 
 def setup(bot):
     if not bot.memory.contains('url_callbacks'):
@@ -80,11 +99,11 @@ def get_arxiv(query):
     title = entry.title
     abstract = no_newlines.sub(' ', entry.summary)
 
-    return (arxivid, authors, title, abstract, abs_link)
+    return (arxivid, authors, title, abstract, httpsify(abs_link))
 
 @commands('arxiv')
 @example('.arxiv 1304.5526')
-def print_summary(bot, input=None, arxiv_id=None):
+def print_summary(bot, input=None, arxiv_id=None, include_link=True):
 
     if arxiv_id is not None:
         query = 'id:' + arxiv_id
@@ -99,21 +118,31 @@ def print_summary(bot, input=None, arxiv_id=None):
     except:
         return bot.say("[arXiv] Could not lookup " + query + " in the arXiv.")
 
-    arxiv_summary = "[arXiv:" + arxiv_id + "] " + authors + ', "' \
-                    + title + '" :: ' + abstract
+    if include_link:
+        arxiv_summary = "[arXiv] " + authors + ', "' \
+                        + title + '" :: ' + abstract
 
-    long_summary = arxiv_summary + " " + url
-    if len(long_summary) > 300:
-        ending = '[…] ' + url
-        clipped = arxiv_summary[:(300-len(ending))] + ending
+        long_summary = arxiv_summary + " " + url
+        if len(long_summary) > 300:
+            ending = '[…] ' + url
+            clipped = arxiv_summary[:(300-len(ending))] + ending
+        else:
+            clipped = long_summary
     else:
-        clipped = long_summary
+        arxiv_summary = "[arXiv:" + arxiv_id + "] " + authors + ', "' \
+                        + title + '" :: ' + abstract
+
+        if len(arxiv_summary) > 300:
+            ending = '[…]'
+            clipped = arxiv_summary[:(300-len(ending))] + ending
+        else:
+            clipped = arxiv_summary
 
     bot.say(clipped)
 
 @rule('.*(arxiv:|xxx\.lanl\.gov\/[a-z]+\/|arxiv\.org\/[a-z]+\/)(\d{4}\.\d{4,5}|[a-z\-\.]+/\d{7}).*')
 def info(bot, trigger, found_match=None):
     match = found_match or trigger
-    print_summary(bot, arxiv_id=match.group(2))
+    print_summary(bot, arxiv_id=match.group(2), include_link=False)
 
 
